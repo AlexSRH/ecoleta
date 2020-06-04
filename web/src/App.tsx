c3b38c4b9b0eb9e9a4dacee958b99eec88b9feae
Alexsandro G Bezerra <aalexo1313@gmail.com>
2020-06-04 14:10:40 -0300

    aula 03

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello, World!</h1>
-      </header>
-    </div>
-  );
-}
+import Routes from './routes'
 
-export default App;
+const App: React.FC = () => <Routes />
+
+export default App
